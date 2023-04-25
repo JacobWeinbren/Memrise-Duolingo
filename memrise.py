daa8d51cab37ba7e0446981cf0e4c89be1447bd2
Jacob Weinbren <jacobweinbren@gmail.com>
2023-04-25 23:26:16 +0100

    Added in audio files

--- memrise.py
+++ memrise.py
@@ -1,27 +1,72 @@
-import os, credentials
-import azure.cognitiveservices.speech as speechsdk
+import os, re
+import csv
+from google.cloud import texttospeech
+from google.oauth2 import service_account
 
-# This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-speech_config = speechsdk.SpeechConfig(subscription=credentials.key, region='uksouth')
-audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+# Load the credentials from the credentials.json file
+credentials = service_account.Credentials.from_service_account_file('credentials.json')
 
-# The language of the voice that speaks.
-speech_config.speech_synthesis_voice_name='	he-IL-AvriNeural'
+# Create a TextToSpeechClient with the loaded credentials
+client = texttospeech.TextToSpeechClient(credentials=credentials)
 
-speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+# Build the voice request, select the language code ("he-IL") and the ssml gender ("neutral")
+voice = texttospeech.VoiceSelectionParams(
+    language_code="he-IL", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+)
 
-# Get text from the console and synthesize to the default speaker.
-print("Enter some text that you want to speak >")
-text = input()
+# Select the type of audio file you want returned
+audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.MP3
+)
 
-speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+"""Generate TTS audio for a word and save it to a file using Google TTS."""
+def get_sound(word, meaning, file, index):
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=word)
 
-if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-    print("Speech synthesized for text [{}]".format(text))
-elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
-    cancellation_details = speech_synthesis_result.cancellation_details
-    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
-    if cancellation_details.reason == speechsdk.CancellationReason.Error:
-        if cancellation_details.error_details:
-            print("Error details: {}".format(cancellation_details.error_details))
-            print("Did you set the speech resource key and region values?")
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    # The response's audio_content is binary.
+    with open(os.path.join(file, f"{index}-{word}.mp3"), "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+
+    print(f"Generated TTS audio for: {word}, {meaning} using Google TTS")
+
+
+# Read the CSV file as a list of dictionaries
+csv_file = 'words.csv'
+with open(csv_file, mode='r', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = [row for row in reader]
+
+categories = {}
+
+# Group words by category
+for item in data:
+    if item['Title'] not in categories:
+        categories[item['Title']] = []
+    categories[item['Title']].append([item['Word'], item['Meaning']])
+
+# Loop through categories and create output folders
+for index, category in enumerate(categories):
+    output_folder = 'storage/' + str(index) + ' - ' + category
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Write category data to CSV
+    with open(output_folder + '/' + category + '.csv', 'w') as out:
+        csv_out = csv.writer(out)
+        csv_out.writerow(['Word', 'Meaning'])
+        csv_out.writerows(categories[category])
+
+    # Generate TTS audio for each word in the category
+    for index, word in enumerate(categories[category]):
+        simplified_word = re.sub("[\(\[].*?[\)\]]", "", word[0]).strip()
+        meaning = re.sub("[\(\[].*?[\)\]]", "", word[1]).strip()
+        output_file = os.path.join(output_folder, f"{index}-{simplified_word}.mp3")
+        if not os.path.exists(output_file):
+            get_sound(simplified_word, meaning, output_folder, index)
